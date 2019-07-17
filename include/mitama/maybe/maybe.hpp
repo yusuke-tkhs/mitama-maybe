f571b5bbd3d94fe19a897bf8a6d192d6a6d348da
Mitama <loligothick@gmail.com>
2019-07-18 02:18:45 +0900

    initial commit

---
+++ include/mitama/maybe/maybe.hpp
@@ -0,0 +1,174 @@
+#ifndef MITAMA_MAYBE_HPP
+#define MITAMA_MAYBE_HPP
+
+#include <type_traits>
+#include <optional>
+#include <utility>
+#include <memory>
+#include <functional>
+#include <boost/optional.hpp>
+
+namespace mitama::mitamagic {
+template <class, class=void> struct is_pointer_like: std::false_type {};
+
+template <class PointerLike>
+struct is_pointer_like<PointerLike,
+    std::void_t<
+        decltype(std::declval<PointerLike&>().operator->()),
+        decltype(*std::declval<PointerLike&>()),
+        decltype(bool(std::declval<PointerLike&>()))>>
+    : std::true_type
+{};
+
+template <class>
+struct element_type;
+
+template <class T>
+struct element_type<std::shared_ptr<T>> {
+    using type = T;
+};
+template <class T>
+struct element_type<std::unique_ptr<T>> {
+    using type = T;
+};
+template <class T>
+struct element_type<std::optional<T>> {
+    using type = T;
+};
+template <class T>
+struct element_type<boost::optional<T>> {
+    using type = T;
+};
+template <class T>
+struct element_type<T*> {
+    using type = T;
+};
+}
+
+namespace mitama::mitamagic {
+
+template <class T>
+class polymorphic_storage {
+  public:
+    using pointer_type = T*;
+    using pointer_const_type = const T*;
+    using reference_type = T&;
+
+    virtual ~polymorphic_storage() = default;
+    virtual T& deref() = 0;
+    virtual bool is_ok() const = 0;
+    virtual pointer_type get_pointer() = 0;
+    virtual pointer_const_type get_pointer() const  = 0;
+};
+
+template <class, class=void>
+class maybe_view;
+
+template <class T>
+class maybe_view<T,
+    std::enable_if_t<
+        std::disjunction_v<
+            mitamagic::is_pointer_like<std::remove_reference_t<T>>,
+            std::is_pointer<std::remove_reference_t<T>>>>>
+    : public polymorphic_storage<typename mitamagic::element_type<std::decay_t<T>>::type>
+{
+    T storage_;
+  public:
+    using reference_type = typename polymorphic_storage<typename mitamagic::element_type<std::decay_t<T>>::type>::reference_type;
+    using pointer_type = typename polymorphic_storage<typename mitamagic::element_type<std::decay_t<T>>::type>::pointer_type;
+    using pointer_const_type = typename polymorphic_storage<typename mitamagic::element_type<std::decay_t<T>>::type>::pointer_const_type;
+
+    virtual ~maybe_view() = default;
+    maybe_view() = delete;
+
+    template <typename U, std::enable_if_t<std::is_convertible_v<U,T>, bool> = false>
+    maybe_view(U&& u) : storage_{std::forward<U>(u)} {}
+
+    bool is_ok() const override final {
+        return bool(storage_);
+    }
+    reference_type deref() override final {
+        return *storage_;
+    }
+    pointer_type get_pointer() override final {
+        if constexpr (std::is_pointer_v<std::remove_reference_t<T>>) {
+            return storage_;
+        }
+        else {
+            return storage_.operator->();
+        }
+    }
+    pointer_const_type get_pointer() const override final {
+        if constexpr (std::is_pointer_v<std::remove_reference_t<T>>) {
+            return storage_;
+        }
+        else {
+            return storage_.operator->();
+        }
+    }
+};
+
+template <class T> maybe_view(T&&) -> maybe_view<T>;
+}
+
+namespace mitama {
+
+template <class T>
+class maybe {
+    std::unique_ptr<mitamagic::polymorphic_storage<T>> storage_;
+  public:
+    
+    template <typename U>
+    maybe(U&& u) : storage_(new mitamagic::maybe_view(std::forward<U>(u))) {}
+
+    template <class F,
+        std::enable_if_t<
+            std::is_invocable_v<F&&, T&>,
+        bool> = false>
+    auto operator>>(F&& f) const& {
+        static_assert(std::is_invocable_v<F&&, T&>,
+            "specified functor is not invocable.");
+        if constexpr (std::is_constructible_v<std::invoke_result_t<F&&, T&>, decltype(nullptr)>) {
+            using result_type = std::invoke_result_t<F&&, T&>;
+            if ( storage_->is_ok() ) {
+                return maybe<typename mitamagic::element_type<std::decay_t<result_type>>::type>{std::invoke(std::forward<F>(f), storage_->deref())};
+            }
+            else {
+                return maybe<typename mitamagic::element_type<std::decay_t<result_type>>::type>{result_type{nullptr}};
+            }
+        }
+        else {
+            using result_type = std::invoke_result_t<F&&, T&>;
+            if ( storage_->is_ok() ) {
+                return maybe<result_type>{boost::optional<result_type>{std::invoke(std::forward<F>(f), storage_->deref())}};
+            }
+            else {
+                return maybe<result_type>{boost::optional<result_type>{boost::none}};
+            }
+        }
+    }
+    
+    explicit operator bool() const {
+        return storage_->is_ok();
+    }
+    
+    decltype(auto) unwrap() & {
+        return storage_->deref();
+    }
+
+    decltype(auto) unwrap() const& {
+        return storage_->deref();
+    }
+
+    decltype(auto) operator->() & {
+        return storage_->get_pointer();
+    }
+
+    decltype(auto) operator->() const& {
+        return storage_->get_pointer();
+    }
+};
+
+template <class T> maybe(T&&) -> maybe<typename mitamagic::element_type<std::decay_t<T>>::type>;
+}
+#endif
